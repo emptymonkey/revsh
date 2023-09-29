c39d068b136ff5bf5cf3243f5c9ec8e740e20ec6
gongqianye241 <gongqianye241>
2023-09-29 14:52:36 -0400

    Fix base64 length calculation

--- io_ssl.c
+++ io_ssl.c
@@ -1275,7 +1275,7 @@ int init_io_target(){
 				strcat(credential, username);
 				strcat(credential, ":");
 				strcat(credential, password);
-				char *credential_b64 = calloc(1, ((strlen(credential) / 3) * 4) + 1);
+				char *credential_b64 = calloc(1, (((strlen(credential) + 2) / 3) * 4) + 1);
 				EVP_EncodeBlock(credential_b64, credential, strlen(credential));
 				free(credential);
 
